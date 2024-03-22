9437bd4493b3fe8f69a1a78bef949826a35a8478
Hernandez Maikol <hernandezdev@proton.me>
2024-03-22 12:40:44 -0400

    Project - main.jsx - adding react router basic configuration

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// react router
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+// router
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <h2>Error element</h2>,
+    children: []
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )

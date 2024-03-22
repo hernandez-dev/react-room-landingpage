6647d1bd348efab903a570cf3b7b7b34c137b9bb
Hernandez Maikol <hernandezdev@proton.me>
2024-03-22 19:08:18 -0400

    Project - App.jsx - adding navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,34 @@
 import Hero from "./components/Hero.jsx"
 import About from "./components/About.jsx"
 
+// links
+const links = [
+  {
+    id: 1,
+    name: "home",
+    to: ""
+  },
+  {
+    id: 2,
+    name: "shop",
+    to: ""
+  },
+  {
+    id: 3,
+    name: "about",
+    to: ""
+  },
+  {
+    id: 4,
+    name: "contact",
+    to: ""
+  }
+]
+
 function App() {
   return (
     <>
-      <Hero />
+      <Hero links={links} />
       <About />
     </>
   )

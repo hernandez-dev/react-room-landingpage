c8da92e37c809f4223d799ea29665b3af62679a8
Hernandez Maikol <hernandezdev@proton.me>
2024-03-22 17:50:27 -0400

    Components - About - applying theme colors to texts

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
       </div>
       <div className="flex col-span-6 px-14">
         <div className="m-auto">
-          <h2 className="font-bold text-gray-700 uppercase leading-none tracking-[.5rem]">
+          <h2 className="font-bold text-very-dark-gray uppercase leading-none tracking-[.5rem]">
             about our furniture
           </h2>
-          <p className="mt-5 text-lg text-gray-500 leading-8">
+          <p className="mt-5 text-lg text-dark-gray leading-8">
             Our multifunctional collection blends design and function to suit your individual taste. Make each room unique, or pick a cohesive theme that best express your interests and what inspires you. Find the furniture prieces you need, from traditional to contemporary styles or anything in between. Product specialists are available to help you create your dream space.
           </p>
         </div>

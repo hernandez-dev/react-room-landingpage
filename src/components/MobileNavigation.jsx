c04d2a35831ff0a9ee911e87d1bb94f31782e148
Hernandez Maikol <hernandezdev@proton.me>
2024-03-23 12:46:09 -0400

    Components - MobileNavigation - hidding mobile navigation using dispatch context

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -1,10 +1,17 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
 
+// context
+import DispatchContext from "../context/DispatchContext.jsx"
+
 export default function MobileNavigation({ links }) {
+  // dispatch cotext
+  const dispatch = useContext(DispatchContext)
+
   return(
     <div className="absolute top-0 left-0 z-10 w-full h-full bg-black/50 desktop:hidden">
       <header className="flex items-center justify-between p-10 bg-white">
-        <button className="flex items-center justify-center w-10 h-10">
+        <button onClick={() => dispatch({ type: "show-mobile-navigation", value: false })} className="flex items-center justify-center w-10 h-10">
           <img src="/images/icon-close.svg" alt="room logo" />
         </button>
         <nav className="font-bold text-very-dark-gray">

84e2b541322752165ecd50af59fee51bdc88d865
Hernandez Maikol <hernandezdev@proton.me>
2024-03-23 12:44:38 -0400

    Project - App.jsx - adding initial state using immer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useImmerReducer } from "use-immer"
+
 // context
 import StateContext from "./context/StateContext.jsx"
 import DispatchContext from "./context/DispatchContext.jsx"
@@ -8,7 +10,7 @@ import About from "./components/About.jsx"
 import MobileNavigation from "./components/MobileNavigation.jsx"
 
 // links
-const links = [
+/*const links = [
   {
     id: 1,
     name: "home",
@@ -29,15 +31,53 @@ const links = [
     name: "contact",
     to: ""
   }
-]
+]*/
 
 function App() {
+  // initialState
+  const initialState = {
+    links: [
+      {
+        id: 1,
+        name: "home",
+        to: ""
+      },
+      {
+        id: 2,
+        name: "shop",
+        to: ""
+      },
+      {
+        id: 3,
+        name: "about",
+        to: ""
+      },
+      {
+        id: 4,
+        name: "contact",
+        to: ""
+      }
+    ],
+    showMobileNavigation: false
+  }
+
+  // reducer
+  function reducer(draft, action) {
+    switch(action.type) {
+      case "show-mobile-navigation":
+        draft.showMobileNavigation = action.value
+        break
+    } // switch end
+  }
+
+  const  [state, dispatch] = useImmerReducer(reducer, initialState)
+
   return (
-    <StateContext.Provider value={1}>
-      <DispatchContext.Provider value={1}>
-        <Hero links={links} />
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        <Hero links={state.links} />
         <About />
-        <MobileNavigation links={links} />
+        {state.showMobileNavigation ? <MobileNavigation links={state.links} /> : ""}
       </DispatchContext.Provider>
     </StateContext.Provider>
   )

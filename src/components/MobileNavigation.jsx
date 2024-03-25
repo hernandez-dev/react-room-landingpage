11b7097945cd7ec4e1177012f587d94ca94ffdea
Hernandez Maikol <hernandezdev@proton.me>
2024-03-25 10:44:26 -0400

    Components - MobileNavigation - pulling nodoRef from props ( CSSTransition )

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
 // context
 import DispatchContext from "../context/DispatchContext.jsx"
 
-export default function MobileNavigation({ links }) {
+export default function MobileNavigation({ nodeRef, links }) {
   // dispatch cotext
   const dispatch = useContext(DispatchContext)
 
   return(
-    <div className="absolute top-0 left-0 z-10 w-full h-full bg-black/50 desktop:hidden">
+    <div ref={nodeRef} className="absolute top-0 left-0 z-10 w-full h-full bg-black/50 desktop:hidden">
       <header className="flex items-center justify-between p-10 bg-white">
         <button onClick={() => dispatch({ type: "show-mobile-navigation", value: false })} className="flex items-center justify-center w-10 h-10">
           <img src="/images/icon-close.svg" alt="room logo" />
